f5b36253aa70a48b9ea850c60ed2a3c3e0ff60f1
Robin.Chao <zhangyongchao@mrocker.com>
2014-11-15 17:50:15 +0800

    优化crash捕获能力

---
+++ mPush_iOS_sdk_v1.3.3/Demo/mPushDemo/DemoViewController.h
@@ -0,0 +1,12 @@
+//
+//  DemoViewController.h
+//  mPushDemo
+// 
+//  Copyright (c) 2014年 mRocker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoViewController : UIViewController
+
+@end
